a8d2d5c20f266ff04fc4513598d5fd27085aec43
Anthony Gacis (@ageesea) <anthony.gacis@yahoo.com>
2022-11-20 12:06:47 +0800

    Move to env

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -8,12 +8,12 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-    authDomain: "github-star-hub.firebaseapp.com",
-    projectId: "github-star-hub",
-    storageBucket: "github-star-hub.appspot.com",
-    messagingSenderId: "985580287799",
-    appId: "1:985580287799:web:bd0d5b466fdb43a2528cb2",
-    measurementId: "G-LWEHZKZVR6",
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
